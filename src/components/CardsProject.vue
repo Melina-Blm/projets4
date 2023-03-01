80ccb94a00d6e859578c98dbed829241187c1e4d
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-03-01 23:43:42 +0100

    commit correction bug

--- src/components/CardsProject.vue
+++ src/components/CardsProject.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center flex-wrap my-6 gap-3 lg:gap-7 hover:bg-greyhoverpo lg:p-5 transition-all duration-700 hover:scale-105 ease-in-out">
+    <div class="flex items-center flex-wrap my-6 gap-3 lg:gap-7 hover:bg-greyhoverpo lg:p-5 px-3 transition-all duration-700 hover:scale-105 ease-in-out">
     <img class=" md:max-w-xs laptoplarge:max-w-lg lg:max-w-sm rounded-md transition-all hover:opacity-90" src="../assets/askme.jpg" alt="">
     <div class="text-white font-poppins md:max-w-xs lg:max-w-md  laptoplarge:max-w-lg laptopmax:max-w-xl">
       <p class="text-white tracking-widexl font-light text-base lg:text-2xl lg:font-extralight">ASKME</p>
